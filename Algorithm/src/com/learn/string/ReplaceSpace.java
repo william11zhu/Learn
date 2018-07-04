905a19c3f754b85d0d6f9036c53d57c4930c1b95
william11zhu <william11zhu@qq.com>
2018-07-04 09:43:24 +0800

    替换字符串中的空格

---
+++ Algorithm/src/com/learn/string/ReplaceSpace.java
@@ -0,0 +1,33 @@
+package com.learn.string;
+
+public class ReplaceSpace {
+	public String replace(char[] cString){
+		if(cString == null || cString.length ==0){
+			return null;
+		}
+		int blankCount = 0;
+		int originalLength = 0;
+		for(int i=0;i<cString.length;i++){
+			originalLength++;
+			if(cString[i]==' '){
+				blankCount++;
+			}
+		}
+		int indexOfOriginal = originalLength;
+		int indexOfNew = originalLength + blankCount * 2;
+		if(indexOfNew > cString.length){
+			return null;
+		}
+		while(indexOfOriginal >= 0 && indexOfNew > indexOfOriginal){
+			if(cString[indexOfOriginal] == ' '){
+				cString[indexOfNew--] = '0';
+				cString[indexOfNew--] = '2';
+				cString[indexOfNew--] = '%';
+			}else{
+				cString[indexOfNew--] = cString[indexOfOriginal];
+			}
+			indexOfOriginal--;
+		}
+		return cString.toString();
+	}
+}
