2001e504aaa5855ea4410a7f6f52a170c73520b5
william11zhu <william11zhu@qq.com>
2018-07-05 14:29:06 +0800

    索引二叉树寻找中序遍历后续节点

--- Algorithm/src/com/learn/tree/IndexBinaryTree.java
+++ Algorithm/src/com/learn/tree/IndexBinaryTree.java
@@ -54,4 +54,48 @@ public class IndexBinaryTree {
 			}
 		}
 	}
+	
+	/**
+	 * 中序遍历下一个节点
+	 * @param node
+	 * @return
+	 */
+	public <T> IndexNode<T> getNext(IndexNode<T> node){
+		if(node == null) return null;
+		IndexNode<T> nextNode = null;
+		if(node.getRight() != null){
+			nextNode = node.getRight();	
+			while(nextNode.getLeft() != null){
+				nextNode = nextNode.getLeft();
+			}	
+		}else if(node.getParent() != null){
+			IndexNode<T> current = node;
+			IndexNode<T> parent = node.getParent();
+			while(parent != null && current == parent.getRight()){
+				current = parent;
+				parent = parent.getParent();
+			}
+			nextNode = parent;
+		}
+		return nextNode;
+	}
+	
+	
+	public static void main(String... args){
+		IndexBinaryTree bt = new IndexBinaryTree();
+		IndexNode<Integer> pNode8 = bt.createIndexBinaryTree(8);
+	    IndexNode<Integer> pNode6 = bt.createIndexBinaryTree(6);
+	    IndexNode<Integer> pNode10 = bt.createIndexBinaryTree(10);
+	    IndexNode<Integer> pNode5 = bt.createIndexBinaryTree(5);
+	    IndexNode<Integer> pNode7 = bt.createIndexBinaryTree(7);
+	    IndexNode<Integer> pNode9 = bt.createIndexBinaryTree(9);
+	    IndexNode<Integer> pNode11 = bt.createIndexBinaryTree(11);
+
+	    bt.connectNode(pNode8, pNode6, pNode10);
+	    bt.connectNode(pNode6, pNode5, pNode7);
+	    bt.connectNode(pNode10, pNode9, pNode11);
+
+	    IndexNode<Integer> parent = bt.getNext(pNode7);
+	    System.out.print(parent.getData());
+	}
 }
