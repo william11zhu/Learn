bab658dcb8698342d5d3c94f7711d14a8e98eca0
william11zhu <william11zhu@qq.com>
2018-07-18 15:37:09 +0800

    用栈实现队列

---
+++ Algorithm/src/com/learn/queue/StackToQueue.java
@@ -0,0 +1,47 @@
+package com.learn.queue;
+
+import java.util.Stack;
+
+public class  StackToQueue<T> {
+	private Stack<T> stack1;
+	private Stack<T> stack2;
+	
+	public StackToQueue(){
+		stack1 = new Stack<T>();
+		stack2 = new Stack<T>();
+	}
+	
+	
+	public void appendTail(T node){
+		stack1.push(node);
+	}
+	
+	public void deleteHead(){
+		if(stack2.isEmpty()){
+			while(!stack1.isEmpty()){
+				T t = stack1.pop();
+				stack2.push(t);
+			}
+		}
+		if(stack2.isEmpty()){
+			throw new NullPointerException("The queue is null");
+		}
+		stack2.pop();
+	}
+	
+	public static void main(String... args){
+		StackToQueue<Integer> stq = new StackToQueue<Integer>();
+		
+		for(int i=0;i<5;i++)
+			stq.appendTail(i);
+		System.out.println();
+		stq.deleteHead();
+		stq.deleteHead();
+		System.out.println();
+		for(int i = 5;i < 10;i++)
+			stq.appendTail(i);
+		System.out.println();
+		for(int i =0;i<8;i++)
+			stq.deleteHead();
+	}
+}
